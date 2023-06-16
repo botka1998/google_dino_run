b2a73c54365f4f80a610abfb851cf303642e0e8b
AndrejBotka <andrej.botka@sigmoid.rs>
2023-06-16 14:46:28 +0200

    add templates

--- screen_capture.py
+++ screen_capture.py
@@ -10,13 +10,27 @@ from PySide6.QtWidgets import *
 
 import sys
 
+width = 500
+height = 500
+
 
 class MainApp(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.video_size = QSize(600, 600)
+        self.video_size = QSize(width, height)
         self.setup_ui()
         self.setup_capture()
+        self.t_dino = cv2.cvtColor(cv2.imread("dino.png"), cv2.COLOR_BGR2GRAY)
+        self.t_cactus_1 = template = cv2.cvtColor(
+            cv2.imread("cactus_1.png"), cv2.COLOR_BGR2GRAY
+        )
+        self.t_bird_0 = template = cv2.cvtColor(
+            cv2.imread("bird_0.png"), cv2.COLOR_BGR2GRAY
+        )
+        self.t_bird_1 = template = cv2.cvtColor(
+            cv2.imread("bird_1.png"), cv2.COLOR_BGR2GRAY
+        )
+        # template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
     def handle_x_slider_value_change(self, x):
         self.bounding_box["left"] = int(1920 * x / 100)
@@ -47,31 +61,76 @@ class MainApp(QWidget):
 
     def setup_capture(self):
         """Initialize camera."""
-        self.bounding_box = {"top": 300, "left": 600, "width": 600, "height": 600}
+        self.bounding_box = {"top": 300, "left": 600, "width": 500, "height": height}
         self.sct = mss()
         self.timer = QTimer()
         self.timer.timeout.connect(self.display_video_stream)
         self.timer.start(30)
 
-    def display_video_stream(self):
-        sct_img = np.array(self.sct.grab(self.bounding_box))
-        grey_screen = cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY)
-        template = cv2.imread("dino.png")
-        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    def get_frame(self):
+        self.current_frame = np.array(self.sct.grab(self.bounding_box))
+        return self.current_frame
+
+    def detect_dino(self):
+        dinos = self.detect_object(self.t_dino, self.current_frame, 0.8)
+        w, h = dinos["w"], dinos["h"]
+        for pt in dinos["points"]:
+            cv2.rectangle(
+                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
+            )
+
+    def detect_bird(self):
+        bird_0 = self.detect_object(self.t_bird_0, self.current_frame, 0.6)
+        bird_1 = self.detect_object(self.t_bird_1, self.current_frame, 0.6)
+        w, h = bird_0["w"], bird_0["h"]
+        for pt in bird_0["points"]:
+            cv2.rectangle(
+                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
+            )
+        w, h = bird_1["w"], bird_1["h"]
+        for pt in bird_1["points"]:
+            print("bird", pt)
+            cv2.rectangle(
+                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
+            )
+
+    def detect_cactii(self):
+        cactus = self.detect_object(self.t_cactus_1, self.current_frame, 0.4)
+        w, h = cactus["w"], cactus["h"]
+        for pt in cactus["points"]:
+            cv2.rectangle(
+                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
+            )
+
+    def detect_object(self, template, frame, threshold=0.8):
+        grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         w, h = template.shape[::-1]
-
-        res = cv2.matchTemplate(grey_screen, template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.8
+        res = cv2.matchTemplate(grey_frame, template, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
-        for pt in zip(*loc[::-1]):  # Switch columns and rows
-            # print(pt)
-            cv2.rectangle(sct_img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        return {"points": zip(*loc[::-1]), "w": w, "h": h}
 
+    def display_video_stream(self):
+        # sct_img = np.array(self.sct.grab(self.bounding_box))
+        # grey_screen = cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY)
+        # template = cv2.imread("dino.png")
+        # template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        # w, h = template.shape[::-1]
+
+        # res = cv2.matchTemplate(grey_screen, template, cv2.TM_CCOEFF_NORMED)
+        # threshold = 0.8
+        # loc = np.where(res >= threshold)
+        # for pt in zip(*loc[::-1]):  # Switch columns and rows
+        #     # print(pt)
+        #     cv2.rectangle(sct_img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        self.get_frame()
+        self.detect_dino()
+        self.detect_bird()
+        self.detect_cactii()
         image = QImage(
-            sct_img,
-            sct_img.shape[1],
-            sct_img.shape[0],
-            sct_img.strides[0],
+            self.current_frame,
+            self.current_frame.shape[1],
+            self.current_frame.shape[0],
+            self.current_frame.strides[0],
             QImage.Format_RGB32,
         )
         self.image_label.setPixmap(QPixmap.fromImage(image))
