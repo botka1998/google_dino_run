8d6ea52e1b40b373c14b2eab5186dff53a38a636
botka1998 <57000683+botka1998@users.noreply.github.com>
2023-06-16 20:38:29 +0200

    refactor detection, refactor setup_ui

--- screen_capture.py
+++ screen_capture.py
@@ -20,14 +20,19 @@ class MainApp(QWidget):
     def __init__(self):
         QWidget.__init__(self, None, Qt.WindowStaysOnTopHint)
         self.video_size = QSize(width, height)
+        self.bounding_box = {"top": top, "left": left, "width": 500, "height": height}
         self.setup_ui()
         self.setup_capture()
         self.t_dino = cv2.cvtColor(cv2.imread("dino.png"), cv2.COLOR_BGR2GRAY)
-        self.t_cactus_1 = cv2.cvtColor(cv2.imread("cactus_1.png"), cv2.COLOR_BGR2GRAY)
         self.bird_templates = [
             cv2.cvtColor(cv2.imread(f"bird_{i}.png"), cv2.COLOR_BGR2GRAY)
             for i in range(4)
         ]
+        self.cactii_templates = [
+            cv2.cvtColor(cv2.imread(f"cactus_{i}.png"), cv2.COLOR_BGR2GRAY)
+            for i in range(7)
+        ]
+        self.draw = True
 
     def handle_x_slider_value_change(self, x):
         self.bounding_box["left"] = int(1920 * x / 100)
@@ -37,34 +42,37 @@ class MainApp(QWidget):
 
     def setup_ui(self):
         """Initialize widgets."""
+        self.main_layout = QVBoxLayout()
         self.image_label = QLabel()
         self.image_label.setFixedSize(self.video_size)
+        self.main_layout.addWidget(self.image_label)
+        self.add_sliders()
+        self.quit_button = QPushButton("Quit")
+        self.quit_button.clicked.connect(self.close)
 
+        self.main_layout.addWidget(self.quit_button)
+
+        self.setLayout(self.main_layout)
+
+    def add_sliders(self):
         self.x_pos_slider = QSlider(Qt.Horizontal, self)
         self.x_pos_slider.setValue(int(left * 100 / 1920))
         self.x_pos_slider.valueChanged.connect(self.handle_x_slider_value_change)
         self.y_pos_slider = QSlider(Qt.Horizontal, self)
         self.y_pos_slider.valueChanged.connect(self.handle_y_slider_value_change)
         self.y_pos_slider.setValue(int(top * 100 / 1080))
+        self.slider_label = QLabel("Adjust captrure window position", self)
 
-        self.quit_button = QPushButton("Quit")
-        self.quit_button.clicked.connect(self.close)
-
-        self.main_layout = QVBoxLayout()
-        self.main_layout.addWidget(self.image_label)
-        self.main_layout.addWidget(self.quit_button)
+        self.main_layout.addWidget(self.slider_label)
         self.main_layout.addWidget(self.x_pos_slider)
         self.main_layout.addWidget(self.y_pos_slider)
 
-        self.setLayout(self.main_layout)
-
     def setup_capture(self):
         """Initialize camera."""
-        self.bounding_box = {"top": top, "left": left, "width": 500, "height": height}
         self.sct = mss()
         self.timer = QTimer()
         self.timer.timeout.connect(self.display_video_stream)
-        self.timer.start(30)
+        self.timer.start(15)
 
     def get_frame(self):
         self.current_frame = np.array(self.sct.grab(self.bounding_box))
@@ -73,33 +81,33 @@ class MainApp(QWidget):
         return self.current_frame
 
     def detect_dino(self):
-        dinos = self.detect_object(self.t_dino)
-        w, h = dinos["w"], dinos["h"]
-        for pt in dinos["points"]:
-            cv2.rectangle(
-                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
+        dino = self.detect_object(self.t_dino)
+        if self.draw:
+            self.draw_bounding_box(
+                dino,
             )
 
     def detect_bird(self):
-        detected_birds = map(
-            lambda bird_template: self.detect_object(bird_template), self.bird_templates
-        )
-        for bird in detected_birds:
-            w, h = bird["w"], bird["h"]
-            for pt in bird["points"]:
-                cv2.rectangle(
-                    self.current_frame, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2
-                )
+        detected_birds = map(self.detect_object, self.bird_templates)
+        if self.draw:
+            for bird in detected_birds:
+                self.draw_bounding_box(bird, color=(0, 0, 255))
 
     def detect_cactii(self):
-        cactus = self.detect_object(self.t_cactus_1)
-        w, h = cactus["w"], cactus["h"]
-        for pt in cactus["points"]:
-            cv2.rectangle(
-                self.current_frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2
-            )
+        detected_cactii = map(
+            self.detect_object,
+            self.cactii_templates,
+        )
+        if self.draw:
+            for cactus in detected_cactii:
+                self.draw_bounding_box(cactus, color=(0, 255, 0), thicc=1)
 
-    def detect_object(self, template, threshold=0.8):
+    def draw_bounding_box(self, detected_object, color=(255, 255, 255), thicc=2):
+        w, h = detected_object["w"], detected_object["h"]
+        for pt in detected_object["points"]:
+            cv2.rectangle(self.current_frame, pt, (pt[0] + w, pt[1] + h), color, thicc)
+
+    def detect_object(self, template, threshold=0.85):
         w, h = template.shape[::-1]
         res = cv2.matchTemplate(self.current_grey_frame, template, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
@@ -124,4 +132,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = MainApp()
     win.show()
+    win.setGeometry(1400, 200, win.width(), win.height())
     sys.exit(app.exec_())
